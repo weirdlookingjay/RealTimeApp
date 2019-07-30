b60c54e678bff8ea7b776c917b10590a9ffdfad7
Joe Clark <weirdlookingjay@live.com>
2019-07-30 14:11:53 -0400

    CRUD For Category

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -74,7 +74,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('Update', Response::HTTP_ACCEPTED);
     }
 
     /**
