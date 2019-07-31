7f589ca5f2c8c192aab8de0b03a8ab16a129753d
Joe Clark <weirdlookingjay@live.com>
2019-07-31 14:49:49 -0400

    Vuetify, Vue Router and Initial Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,19 +7,11 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(Vuetify)
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +19,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+
+import router from './Router/router';
+
 const app = new Vue({
     el: '#app',
+    router
 });
